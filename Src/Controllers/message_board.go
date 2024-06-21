293901edf29568382ff44df74027a6ba95c76afd
Zsolt Kovacs <kzsolt82@gmail.com>
2024-06-21 14:56:22 +0300

    feat - board controllers added

--- Src/Controllers/message_board.go
+++ Src/Controllers/message_board.go
@@ -1,33 +1,182 @@
 package Controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"ChatApp/Src/Models"
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"net/http"
+	"time"
+)
 
-func GetAllBoardMessages(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "All board messages!",
+func GetAllBoardMessages(c *gin.Context, collection *mongo.Collection) {
+	var items []bson.M
+	cursor, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error retrieving data from database",
+			"error":   err.Error(),
+		})
+		return
+	}
+	defer cursor.Close(context.Background())
+	if err = cursor.All(context.Background(), &items); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error reading cursor",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, items)
+}
+
+func GetBoardMessageById(c *gin.Context, collection *mongo.Collection) {
+	messageBoardIDParam := c.Param("id")
+	messageBoardID, err := primitive.ObjectIDFromHex(messageBoardIDParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid ID format",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	filter := bson.M{"_id": messageBoardID}
+
+	var messageBoard bson.M
+	if err := collection.FindOne(context.Background(), filter).Decode(&messageBoard); err != nil {
+		if err == mongo.ErrNoDocuments {
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "User not found",
+			})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error retrieving data from database",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, messageBoard)
+}
+
+func AddBoardMessage(c *gin.Context, collection *mongo.Collection) {
+	var newMessageBoard Models.MessageBoard
+	if err := c.BindJSON(&newMessageBoard); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input data"})
+		return
+	}
+	newMessageBoard.CreatedAt = time.Now()
+	newMessageBoard.Liked = nil
+	newMessageBoard.Disliked = nil
+	newMessageBoard.Deleted = nil
+
+	result, err := collection.InsertOne(context.TODO(), newMessageBoard)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Board message added successfully!", "id": result.InsertedID})
+
+}
+
+func UpdateBoardMessage(c *gin.Context, collection *mongo.Collection) {
+	messageBoardIDParam := c.Param("id")
+	messageBoardID, err := primitive.ObjectIDFromHex(messageBoardIDParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid message ID format",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	var updateData Models.MessageBoard
+	if err := c.ShouldBindJSON(&updateData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid request data",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	updateDoc := bson.M{}
+	fmt.Println(updateData)
+	if updateData.MessageFrom != "" {
+		updateDoc["messageFrom"] = updateData.MessageFrom
+	}
+	if updateData.Content != "" {
+		updateDoc["content"] = updateData.Content
+	}
+
+	// fix this later (like and dislike, deleted)
+	if updateData.Deleted != nil {
+		updateDoc["deleted"] = 2
+	}
+	if updateData.Liked != nil {
+		updateDoc["liked"] = 3
+	}
+	if updateData.Disliked != nil {
+		updateDoc["disliked"] = true
+	}
+
+	update := bson.M{"$set": updateDoc}
+	filter := bson.M{"_id": messageBoardID}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to update board message",
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	if result.MatchedCount == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No board message found with given ID",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Board message updated successfully!",
 	})
 }
 
-func GetBoardMessageById(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Get board message by id!",
-	})
-}
+func DeleteBoardMessageById(c *gin.Context, collection *mongo.Collection) {
+	messageBoardIDParam := c.Param("id")
+	messageBoardID, err := primitive.ObjectIDFromHex(messageBoardIDParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid ID format",
+			"error":   err.Error(),
+		})
+		return
+	}
 
-func AddBoardMessage(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Board message added!",
-	})
-}
+	result, err := collection.DeleteOne(context.Background(), bson.M{"_id": messageBoardID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to delete board message",
+			"error":   err.Error(),
+		})
+		return
+	}
 
-func UpdateBoardMessage(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Board message updated!",
-	})
-}
+	if result.DeletedCount == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "No board message found with given ID",
+		})
+		return
+	}
 
-func DeleteBoardMessage(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Board message deleted!",
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Board message deleted successfully!",
 	})
 }
--- Src/Routes/message_board.go
+++ Src/Routes/message_board.go
@@ -3,14 +3,47 @@ package Routes
 import (
 	"ChatApp/Src/Controllers"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func MessageBoardRoutes(router *gin.Engine) {
+func MessageBoardRoutes(router *gin.Engine, mongoClient *mongo.Client) {
 	v1 := router.Group("/api/v1/")
+	collection := mongoClient.Database("ChatApp").Collection("messageBoards")
 
-	v1.GET("/boardmessage", Controllers.GetAllBoardMessages)
-	v1.GET("/boardmessage/:id", Controllers.GetBoardMessageById)
-	v1.POST("/boardmessage", Controllers.AddBoardMessage)
-	v1.PATCH("/boardmessage/:id", Controllers.UpdateBoardMessage)
-	v1.DELETE("/boardmessage/:id", Controllers.DeleteBoardMessage)
+	v1.GET("/boardmessage", getAllBoardMessages(collection))
+	v1.GET("/boardmessage/:id", getBoardMessageById(collection))
+	v1.POST("/boardmessage", addBoardMessage(collection))
+	v1.PATCH("/boardmessage/:id", updateBoardMessage(collection))
+	v1.DELETE("/boardmessage/:id", deleteBoardMessage(collection))
+
+}
+
+func getAllBoardMessages(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Controllers.GetAllBoardMessages(c, collection)
+	}
+}
+
+func getBoardMessageById(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Controllers.GetBoardMessageById(c, collection)
+	}
+}
+
+func addBoardMessage(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Controllers.AddBoardMessage(c, collection)
+	}
+}
+
+func updateBoardMessage(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Controllers.UpdateBoardMessage(c, collection)
+	}
+}
+
+func deleteBoardMessage(collection *mongo.Collection) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		Controllers.DeleteBoardMessageById(c, collection)
+	}
 }
